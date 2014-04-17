67a02bcba0187b5c4a9caed88d9e72ba16a7f538
Evan Travers <evantravers@gmail.com>
2014-04-17 16:28:11 -0500

    restrict to pipers posts

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -2,8 +2,8 @@ var feed = new Instafeed({
   get: 'tagged',
     tagName: 'piperzebras',
     resolution: 'standard_resolution',
-    userId: '',
+    userId: '16206566',
     clientId: 'c0c7115af26f418c87e7be22c303da2f',
-    token: ''
+    accesToken: '16206566.467ede5.f514a92579754e0298fa023cf7848573'
 });
 feed.run();
