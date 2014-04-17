af3bf1dae268d590a3cda09183f95099fd55f140
Evan Travers <evantravers@gmail.com>
2014-04-17 16:23:48 -0500

    initial commit

---
+++ source/javascripts/main.js
@@ -0,0 +1,9 @@
+var feed = new Instafeed({
+  get: 'tagged',
+    tagName: 'piperzebras',
+    resolution: 'standard_resolution',
+    userId: '',
+    clientId: 'c0c7115af26f418c87e7be22c303da2f',
+    token: ''
+});
+feed.run();
