fb1014b8a9a7f51fc9ee1c9882a0fd4cd72ae041
Evan Travers <evantravers@gmail.com>
2016-12-04 14:44:13 -0600

    update accesstoken variable?

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -15,7 +15,7 @@ $('document').ready(function() {
     resolution: 'standard_resolution',
     userId: '16206566',
     clientId: 'c0c7115af26f418c87e7be22c303da2f',
-    accesToken: '16206566.467ede5.f514a92579754e0298fa023cf7848573',
+    accessToken: '16206566.467ede5.f514a92579754e0298fa023cf7848573',
     before: function() {
       $('#instafeed').waypoint('disable');
     },
